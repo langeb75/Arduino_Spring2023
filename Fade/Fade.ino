dfcbdc6da1ce1c74d80aa2f917ffb8f5174259a7
Mr. Lange <blange@gulllakecs.org>
2023-03-14 14:14:42 -0400

    Added ledEleven Variable - 10 & 11 Fading - Stable

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -18,20 +18,23 @@
   http://www.arduino.cc/en/Tutorial/Fade
 */
 
-int led = 10;           // the PWM pin the LED is attached to
+int ledTen = 10;       // Pin 10 utilizing PWM
+int ledEleven = 11;    // Pin 11 utilizing PWM
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(ledTen, OUTPUT);
+  pinMode(ledEleven, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
-  analogWrite(led, brightness);
+  analogWrite(ledTen, brightness);
+  analogWrite(ledEleven, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
