c7c76eb34d0923cc9802c07b5b555c7a8bdcb51e
Mr. Lange <blange@gulllakecs.org>
2023-03-16 14:07:42 -0400

    Initial Blind Spot Monitor Commit

---
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -0,0 +1,19 @@
+//Programmer: Mr. Lange
+//Date: 3.16.2023
+//Program: Blind Spot Monitor
+
+/*
+ * This program will determine distances approaching 
+ * vehicle that are in your blind spot and turn on a warning light
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
